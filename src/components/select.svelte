86fb355838ac15c2cc916394985f93a322d029b0
sacarvy <sukhpreetben10@gmail.com>
2021-12-10 18:08:41 +0530

    reuseable forms component

---
+++ src/components/select.svelte
@@ -0,0 +1,13 @@
+							<fw-select
+							label="Therapy Name"
+							required="true"
+							value="1"
+							
+							placeholder="Your choice"
+							state-text="Select singluar option"
+							>
+							<fw-select-option value="1">Starks</fw-select-option>
+							<fw-select-option value="2">Lannisters</fw-select-option>
+						</fw-select>
+<script lang="ts">
+</script>
---
+++ src/components/text_area.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let placeholder;
+  export let type = 'text';
+  export let id;
+  export let rows = 3;
+  export let name;
+  export let autocomplete = 'off';
+  export let clx="";
+</script>
+<div class={`relative ${clx}`}>
+	<textarea
+		type={type}
+		id={id}
+		name={name}
+		placeholder={placeholder}
+		autocomplete={autocomplete}
+		rows={rows}
+		class="relative z-10 w-full p-2 text-base placeholder-transparent bg-transparent border-2 border-gray-300 rounded-lg peer text-blueGray-300 focus:outline-none focus:border-indigo-600"
+	/>
+  <label
+		class="absolute z-0 text-sm text-gray-600 transition-all left-2 -top-6 peer-placeholder-shown:text-gray-300 peer-placeholder-shown:text-base peer-placeholder-shown:top-2 peer-focus:-top-6 peer-focus:text-gray-600 peer-focus:text-sm"
+		for={name}>{placeholder}</label
+	>
+</div>
